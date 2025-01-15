e72a24ec4b0d0657f40c2699e306f8deecd9cf82
choheejin <hjcho9510@gmail.com>
2025-01-15 09:42:20 +0900

    chore(sql): init.sql 설정 변경 - #S12P11E103-109

--- backend/src/main/resources/init.sql
+++ backend/src/main/resources/init.sql
@@ -1,17 +1,54 @@
--- user 테이블 생성
-CREATE TABLE IF NOT EXISTS user (
-    id INT PRIMARY KEY,
-    department VARCHAR(50),
-    name VARCHAR(50),
-    password VARCHAR(255),
-    position VARCHAR(50),
-    user_id VARCHAR(50)
+USE ssafy_web_db;
+
+-- Create table for conference_category
+CREATE TABLE conference_category (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
--- conference_category 테이블 생성
-CREATE TABLE IF NOT EXISTS conference_category (
-    id INT PRIMARY KEY,
-    name VARCHAR(50)
+-- Create table for user
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY,
+    department VARCHAR(255),
+    position VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Create table for conference
+CREATE TABLE conference (
+    id INTEGER PRIMARY KEY,
+    owner_id INTEGER NOT NULL,
+    conference_category INTEGER NOT NULL,
+    call_start_time DATETIME,
+    call_end_time DATETIME,
+    thumbnail_url VARCHAR(255),
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    is_active BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (owner_id) REFERENCES user (id),
+    FOREIGN KEY (conference_category) REFERENCES conference_category (id)
+);
+
+-- Create table for user_conference
+CREATE TABLE user_conference (
+    id INTEGER PRIMARY KEY,
+    conference_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (conference_id) REFERENCES conference (id),
+    FOREIGN KEY (user_id) REFERENCES user (id)
+);
+
+-- Create table for conference_history
+CREATE TABLE conference_history (
+    id INTEGER PRIMARY KEY,
+    conference_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    action SMALLINT CHECK (action IN (1, 2, 3)), -- 1: CREATE, 2: JOIN, 3: EXIT
+    inserted_time DATETIME NOT NULL,
+    FOREIGN KEY (conference_id) REFERENCES conference (id),
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 -- user 테이블 초기 데이터 삽입
